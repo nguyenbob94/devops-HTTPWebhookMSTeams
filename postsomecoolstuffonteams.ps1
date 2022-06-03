7f4cc0e72ef8f97c7bba8d57cbfdb52485c0025c
Bob Nguyen <bob.nguyen@oobe.com.au>
2022-06-03 12:36:32 +1000

    Image support

--- postsomecoolstuffonteams.ps1
+++ postsomecoolstuffonteams.ps1
@@ -1,7 +1,7 @@
 $Body = @{
   "Title" = "Automated test message"
-  "Text" = "This message is composed through a Powershell script and scheduled through Azure DevOps"   
-}
+  "Text" = "This message is composed through a Powershell script and scheduled through Azure DevOps. With image in body <img src='https://www.w3schools.com/images/w3schools_green.jpg'>"
+  }
 
 $JsonBody = ConvertTo-Json -Compress -InputObject $Body
 
